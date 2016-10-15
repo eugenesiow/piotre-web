f8e5c92a3c132490004782e80cb21fc114c66b5e
Eugene Siow <kyo116@gmail.com>
2016-10-15 21:33:03 +0100

    Add run script

---
+++ scripts/run.sh
@@ -0,0 +1,1 @@
+java -jar piotre-0.1-jar-with-dependencies.jar -U piotre -P password -D ./db
--- src/main/java/uk/ac/soton/ldanalytics/piotre/server/query/QueryListener.java
+++ src/main/java/uk/ac/soton/ldanalytics/piotre/server/query/QueryListener.java
@@ -32,20 +32,22 @@ public class QueryListener implements UpdateListener {
 				for(Object map:((Map<?, ?>)newEvents[i].getUnderlying()).entrySet()) {
 					Entry<?, ?> entry = ((Entry<?, ?>)map);
 //					System.out.println(entry.getKey()+":"+entry.getValue());
-					String key = entry.getKey().toString();
-					String val = entry.getValue().toString();
-					if(message.has(key)) {
-						JsonElement jVal = message.get(key);
-						JsonArray newVal = new JsonArray();
-						if(jVal.isJsonArray()) {
-							newVal = jVal.getAsJsonArray();
-						} else if(jVal.isJsonPrimitive()) {
-							newVal.add(jVal.getAsString());
+					if(entry.getKey()!=null && entry.getValue()!=null) {
+						String key = entry.getKey().toString();
+						String val = entry.getValue().toString();
+						if(message.has(key)) {
+							JsonElement jVal = message.get(key);
+							JsonArray newVal = new JsonArray();
+							if(jVal.isJsonArray()) {
+								newVal = jVal.getAsJsonArray();
+							} else if(jVal.isJsonPrimitive()) {
+								newVal.add(jVal.getAsString());
+							}
+							newVal.add(val);
+							message.add(key, newVal);
+						} else {
+							message.addProperty(key,val);
 						}
-						newVal.add(val);
-						message.add(key, newVal);
-					} else {
-						message.addProperty(key,val);
 					}
 				}
 			}
